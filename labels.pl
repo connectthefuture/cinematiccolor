c06dd29ccbf99ce85fc09103989f1f866757e211
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-01-16 20:16:47 +1300

    Deploy cf6e03f2ba93d35a286baff1bc93ca02a3734d68 with ghp-import.

--- labels.pl
+++ labels.pl
@@ -1,11 +1,11 @@
-# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# LaTeX2HTML 2019 (Released January 1, 2019)
 # Associate labels original text with physical files.
 
 
 1;
 
 
-# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# LaTeX2HTML 2019 (Released January 1, 2019)
 # labels from external_latex_labels array.
 
 
